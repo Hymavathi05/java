3ab63448d43c13d3772f36ff608d442d83f2d247
Hymavathi Kotha <kothahymavathi05@gmail.com>
2025-12-15 10:47:22 +0530

    Implement sum of two numbers using user input with Scanner
    This program reads two integer values from user input using the Scanner class and calculates their sum. It introduces input handling, variable storage, arithmetic operations, and output formatting. This task builds the base for interactive programs and reinforces correct input-output flow.

---
+++ basics/Sum_Of_Two.java
@@ -0,0 +1,14 @@
+package com.pack1;
+import java.util.Scanner;
+public class Sum_Of_Two
+	{
+     public static void main(String[] args) {
+     Scanner sc=new Scanner(System.in);
+        	 int x=sc.nextInt();
+        	 int y=sc.nextInt();
+        	 int sum=x+y;
+        	 System.out.println(sum);
+        	 sc.close();
+         }
+	}
+
