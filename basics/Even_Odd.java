6e97544f430a7aa7ac2cb5e0c8a7ed5dd669d299
Hymavathi Kotha <kothahymavathi05@gmail.com>
2025-12-16 21:25:11 +0530

    Add program to check whether a number is even or odd
    This commit adds a Java program that checks whether a given number is
    even or odd using the modulus (%) operator. It helps reinforce basic
    conditional logic, arithmetic operators, and handling edge cases such
    as zero.

---
+++ basics/Even_Odd.java
@@ -0,0 +1,22 @@
+package com.pack1;
+import java.util.Scanner;
+public class Even_Odd {
+   public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter number:");
+		int n=sc.nextInt();
+		if(n==0)
+		{
+			System.out.println("0 is even");
+		}
+		else if(n%2==0)
+		{
+			System.out.println(n + " is Even");
+		}
+		else
+		{
+			System.out.println(n + "is Odd");
+		}
+		sc.close();
+}
+}
